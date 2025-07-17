ac610d09e4dd0d4be05bf138ab81451d85462585
Necdet Güneş <nmehmetgunes@gmail.com>
2025-07-17 17:33:07 +0300

    Plate Counter Eklendi plate eklendi

---
+++ Assets/Scripts/Counters/PlatesCounter.cs
@@ -0,0 +1,42 @@
+using System;
+using UnityEngine;
+
+public class PlatesCounter : BaseCounter
+{
+    public event EventHandler OnPlateSpawned;
+    public event EventHandler OnPlateRemoved;
+
+    [SerializeField] private KitchenObjectsSO plateKitchenObjectSO;
+    [SerializeField] private float spawnPlateTimerMax;
+    [SerializeField] private int platesSpawnedAmountMax;
+    private float spawnPlateTimer;
+    private int platesSpawnedAmount;
+
+    private void Update()
+    {
+        spawnPlateTimer += Time.deltaTime;
+        if (spawnPlateTimer > spawnPlateTimerMax)
+        {
+            spawnPlateTimer = 0f;
+            if (platesSpawnedAmount < platesSpawnedAmountMax )
+            {
+                platesSpawnedAmount++;
+                OnPlateSpawned?.Invoke(this, EventArgs.Empty);
+            }
+        }
+    }
+
+    public override void Interact(Player player)
+    {
+        if (!player.HasKitchenObject())
+        {
+            if(platesSpawnedAmount > 0)
+            {
+                platesSpawnedAmount--;
+                OnPlateRemoved?.Invoke(this, EventArgs.Empty);
+
+                KitchenObject.SpawnKitchenObject(plateKitchenObjectSO, player);
+            }
+        }
+    }
+}
