657648ee44aebd945b4ab6a199c9c9ac6ad7048d
Necdet Güneş <nmehmetgunes@gmail.com>
2025-07-29 19:22:12 +0300

    Tarif sistemi be görev yapısı temelleri eklendi

---
+++ Assets/Scripts/DeliveryManager.cs
@@ -0,0 +1,34 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeliveryManager : MonoBehaviour
+{
+    [SerializeField] private RecipeListSO recipeListSO;
+
+    private List<RecipeSO> waitingRecipeSOList;
+    private float spawnRecipeTimer;
+    private float spawnRecipeTimerMax = 4f;
+    private int waitingRecipesMax = 4;
+
+    private void Awake()
+    {
+        waitingRecipeSOList = new List<RecipeSO>();
+    }
+    private void Update()
+    {
+        spawnRecipeTimer -= Time.deltaTime;
+        if (spawnRecipeTimer < 0f)
+        {
+            spawnRecipeTimer = spawnRecipeTimerMax;
+
+            if (waitingRecipeSOList.Count < waitingRecipesMax)
+            {
+                RecipeSO waitingRecipeSO =  recipeListSO.recipeSOList[Random.Range(0, recipeListSO.recipeSOList.Count)];
+                Debug.Log(waitingRecipeSO.name);
+                waitingRecipeSOList.Add(waitingRecipeSO);
+                Debug.Log(waitingRecipeSOList);
+            }
+        }
+    }
+}
